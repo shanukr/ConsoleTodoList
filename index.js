2eedc0357c9fc567a74de68064470a4d91d63eaf
shanukr <shanu.kumar97@gmail.com>
2021-04-18 00:40:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+"use-strict"
+
+let todoList = []
+
+class Todo {
+    constructor(id, title, completed, createdAt, updatedAt) {
+        this.id = id;
+        this.title = title;
+        this.completed = completed;
+        this.createdAt = createdAt;
+        this.updatedAt = updatedAt;
+    }
+}
+
+Todo.prototype.showTodo = function () {
+    console.log(`-----------------------`)
+    console.log(`TODO ${this.id}: ${this.title}`)
+    this.completed === true ? console.log("Completed") : console.log(`Incomplete`);
+    console.log(`created on ${this.createdAt}`)
+    if (this.updatedAt != null) {
+        console.log(`updated on ${this.updatedAt}`)
+    }
+    console.log(`-----------------------`)
+}
+
+
+let date = () => {
+    let date = new Date();
+    return date.getDate() + `-` + date.getMonth() + `-` + date.getFullYear()
+}
+
+let addTodo = (title) => {
+    todoList.push(new Todo(todoList.length + 1, title, false, date(), null));
+    return `${title} added`
+};
+
+let editTodo = (id, newTitle) => {
+    todoList[id - 1].title = newTitle; todoList[id - 1].updatedAt = date()
+    return `todo ${id} modified to ${newTitle}`;
+};
+
+let completeTodo = (id) => {
+    todoList[id - 1] != undefined ? todoList[id - 1].completed = true : console.log(`No todo with id ${id}`)
+    return `Todo ${id} completed`;
+};
+
+let completeAll = () => {
+    todoList.forEach(todo => { todo.completed = true })
+    return `All todos completed`;
+};
+
+let deleteTodo = (id) => {
+    todoList[id - 1] != undefined ? todoList.splice(id - 1, 1) : console.log(`No Todo with id: ${this.id}`)
+    return `Todo ${id} deleted`;
+};
+
+let clearCompleted = () => {
+    todoList.forEach(todo => todo.completed = false)
+    return `All Todos cleared`;
+};
+
+let getActiveTodoCount = () => {
+    let arr = todoList.filter(todo => todo.completed === true);
+    return arr.length;
+};
+
+let viewTodos = (flag) => {
+    switch (flag) {
+        case 'ALL': todoList.forEach(todo => todo.showTodo()); break;
+        case 'COMPLETE': todoList.forEach(todo => { if (todo.completed === true) todo.showTodo(); }); break;
+        case 'ACTIVE': todoList.forEach(todo => { if (todo.completed === false) todo.showTodo(); }); break;
+        default: console.log("INVALID FLAG!!")
+    }
+};
+
+
+
+
